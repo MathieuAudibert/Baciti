c53df82389397081ba385be200aee84c24d1c80a
MathieuAudibert <mathieu.audibert27@gmail.com>
2024-10-25 10:51:18 +0200

    added connexion to the bucket firebase storage & secrets

---
+++ bucket_co.py
@@ -0,0 +1,13 @@
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials, storage
+
+service_account_info = json.loads(os.getenv('FIREBASE_SERVICE_ACCOUNT'))
+
+cred = credentials.Certificate(service_account_info)
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'baciti2.appspot.com'
+})
+
+bucket = storage.bucket()
